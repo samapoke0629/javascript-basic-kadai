a92317587de6e7aaf2439d0a61b15bf1c1a86d58
key-pro <taka65000@yahoo.co.jp>
2024-07-10 19:49:36 +0900

    kadai_005

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重と身長を変数に代入
+let weight = 70; // 体重 (kg)
+let height = 1.75; // 身長 (m)
+
+// BMIを計算
+let bmi = weight / (height * height);
+
+// 結果を表示
+console.log("体重: " + weight + "kg");
+console.log("身長: " + height + "m");
+console.log("BMI: " + bmi);
